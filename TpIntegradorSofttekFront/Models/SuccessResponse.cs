ac158f614f081d994748f92191c14e3a9e576765
Carlos Franco <cdf.175@gmail.com>
2023-09-30 23:54:32 -0300

    Cambios subidos con los temas de la ultima clase y aplicacion de diseño.

---
+++ TpIntegradorSofttekFront/Models/SuccessResponse.cs
@@ -0,0 +1,16 @@
+﻿namespace TpIntegradorSofttekFront.Models
+{
+	public class SuccessResponse<T>
+	{
+		public int Status { get; set; }
+		public T? Data { get; set; }
+       
+    }
+
+    public class SuccessResponseList<T> 
+    {
+        public int Status { get; set; }
+        public List<T>? Data { get; set; }
+
+    }
+}
